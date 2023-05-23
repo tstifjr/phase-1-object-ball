597dc8a41212929dbcc34e4c1713c8b8b37bc8dd
TJ Stifter <tjstifter@gmail.com>
2023-05-23 16:05:51 -0400

    added completed game object function

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -56,10 +56,65 @@ function gameObject() {
             }, 
          },
     },
-    away: {},
+    away: {
+        teamName: "Charlotte Hornets",
+        colors: ["Turquoise" , "Purple"],
+        players:{
+        "Jeff Adrian": {
+            "number": 4,
+            "shoe": 18,
+            "points": 10,
+            "rebounds": 1,
+            "assists": 1,
+            "steals": 2,
+            "blocks": 7,
+            "slamDunks": 2,
+        },
+        "Bismak Biyombo": {
+            "number": 0,
+            "shoe": 16,
+            "points": 12,
+            "rebounds": 4,
+            "assists": 7,
+            "steals": 7,
+            "blocks": 15,
+            "slamDunks": 10,
+        },
+        "DeSagna Diop": {
+            "number": 2,
+            "shoe": 14,
+            "points": 24,
+            "rebounds": 12,
+            "assists": 12,
+            "steals": 4,
+            "blocks": 5,
+            "slamDunks": 5,
+        },
+        "Ben Gordon": {
+            "number": 8,
+            "shoe": 15,
+            "points": 33,
+            "rebounds": 3,
+            "assists": 2,
+            "steals": 1,
+            "blocks": 1,
+            "slamDunks": 0,
+        },
+        "Brendan Haywood": {
+            "number": 33,
+            "shoe": 15,
+            "points": 6,
+            "rebounds": 12,
+            "assists": 12,
+            "steals": 22,
+            "blocks": 5,
+            "slamDunks": 12,
+        }, 
+     },
+},
 
    };
     
     return object;
 }
-console.log(gameObject().home.players);
+console.log(gameObject());
